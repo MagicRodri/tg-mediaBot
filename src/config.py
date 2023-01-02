459219091c0e1b22e82beaa7d427cb42ef90969d
Rodrigue Roland CODO <codorol22@gmail.com>
2023-01-02 20:53:22 +0500

    Config updated for mongoDB and cloudinary

--- src/config.py
+++ src/config.py
@@ -1,6 +1,7 @@
 import os
 from pathlib import Path
 
+import cloudinary
 from dotenv import load_dotenv
 
 # Absolute path to the project directory
@@ -9,4 +10,23 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Load environment variables from .env file
 load_dotenv(dotenv_path=BASE_DIR / ".env")
 
+# General configuration
+LOCAL_FILES_SAVING = True
+CLOUD_FILES_SAVING = True
+
+# Telegram bot token
 TG_TOKEN = os.getenv("TG_TOKEN")
+
+# MongoDB configuration
+MG_URI = os.getenv("MG_URI")
+
+# Cloudinary configuration
+CLOUDINARY_CLOUD_NAME = os.getenv("CLOUDINARY_CLOUD_NAME")
+CLOUDINARY_API_KEY = os.getenv("CLOUDINARY_API_KEY")
+CLOUDINARY_API_SECRET = os.getenv("CLOUDINARY_API_SECRET")
+cloudinary.config(
+    cloud_name=CLOUDINARY_CLOUD_NAME,
+    api_key=CLOUDINARY_API_KEY,
+    api_secret=CLOUDINARY_API_SECRET,
+    secure=True,
+)
